c076e86c9e06626b88cc33e19c631b121f495a71
KD-MM2 <kaotd.z3@gmail.com>
2024-07-14 10:51:54 +0900

    feat: Update dependencies, add note-related files, and improve views

--- Trilium Client/Components/NoteContentView.swift
+++ Trilium Client/Components/NoteContentView.swift
@@ -8,10 +8,12 @@
 import MarkupEditor
 import SwiftUI
 
+
 struct NoteContentView: View {
     @ObservedObject var notesViewModel: NotesViewModel
     var note: Note
     @State var noteContent = NSAttributedString(string: "Note empty")
+    @State var noteString: String = ""
     @State private var isLoading = true
     
     var body: some View {
@@ -19,17 +21,20 @@ struct NoteContentView: View {
             if isLoading {
                 ProgressView("Loading...")
             } else {
+//                MarkupEditorView(html: $noteString)
                 RichTextEditor(text: $noteContent)
                     .cornerRadius(5)
+//                    .frame(width: 500)
             }
         }
         .padding()
         .background(Color.gray.opacity(0.3))
-        .ignoresSafeArea(.keyboard, edges: .bottom)
+//        .ignoresSafeArea(.keyboard, edges: .bottom)
+//        .ignoresSafeArea(.keyboard)
         .task {
             TriliumAPI.shared.getNoteContent(noteId: note.noteId, completion: { content in
                 switch content {
-                case let .success(response):                    
+                case let .success(response):
                     // Safely unwrap data from the response string
                     if let data = (response as String).data(using: .utf8, allowLossyConversion: true) {
                         do {
@@ -53,7 +58,7 @@ struct NoteContentView: View {
                     } else {
                         print("Error: Data conversion to UTF-8 failed.")
                     }
-                    
+                    noteString = response
                     isLoading = false
                 case let .failure(error):
                     print("Error: \(error)")
--- Trilium Client/Libraries/RichTextEditor.swift
+++ Trilium Client/Libraries/RichTextEditor.swift
@@ -26,7 +26,6 @@ public extension RichTextView {
 }
 
 typealias ViewRepresentable = UIViewRepresentable
-
 public struct RichTextEditor: ViewRepresentable {
     public init(text: Binding<NSAttributedString>) {
         _attributedString = text
@@ -34,15 +33,149 @@ public struct RichTextEditor: ViewRepresentable {
     
     @Binding
     public var attributedString: NSAttributedString
+    // @Binding public var htmlString: String
+    
+    // public init(htmlString: Binding<String>) {
+    //     _htmlString = htmlString
+    // }
     
     public let textView = RichTextView()
     
-    public func makeUIView(context _: Context) -> some UIView {
+    public func makeUIView(context: Context) -> some UIView {
         textView.attributedString = attributedString
-        return textView
+        textView.delegate = context.coordinator
+        
+        let toolbar = UIHostingController(rootView: StyleToolbar(
+            onBold: { self.applyStyle(.bold) },
+            onItalic: { self.applyStyle(.italic) },
+            onUnderline: { self.applyStyle(.underline) },
+            onStrikethrough: { self.applyStyle(.strikethrough) }
+        )).view!
+        
+        let container = UIStackView(arrangedSubviews: [toolbar, textView])
+        container.axis = .vertical
+        
+        return container
     }
     
     public func updateUIView(_: UIViewType, context _: Context) {}
+    
+    public func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func applyStyle(_ style: TextStyle) {
+        let selectedRange = textView.selectedRange
+        
+        // If no text is selected, return
+        if selectedRange.length == 0 { return }
+        
+        let currentAttributes = textView.attributedText.attributes(at: selectedRange.location, effectiveRange: nil)
+        var newAttributes: [NSAttributedString.Key: Any] = [:]
+        
+        switch style {
+        case .bold:
+            if let font = currentAttributes[.font] as? UIFont {
+                newAttributes[.font] = font.isBold ? font.withoutBold() : font.withBold()
+            }
+        case .italic:
+            if let font = currentAttributes[.font] as? UIFont {
+                newAttributes[.font] = font.isItalic ? font.withoutItalic() : font.withItalic()
+            }
+        case .underline:
+            if let underlineStyle = currentAttributes[.underlineStyle] as? Int {
+                // If underline is present, remove it
+                newAttributes[.underlineStyle] = nil
+            } else {
+                // If no underline, add it
+                newAttributes[.underlineStyle] = NSUnderlineStyle.single.rawValue
+            }
+        case .strikethrough:
+            if let strikethroughStyle = currentAttributes[.strikethroughStyle] as? Int {
+                // If strikethrough is present, remove it
+                newAttributes[.strikethroughStyle] = nil
+            } else {
+                // If no strikethrough, add it
+                newAttributes[.strikethroughStyle] = NSUnderlineStyle.single.rawValue
+            }
+        }
+        
+        if style == .underline {
+            // For underline, we need to remove the attribute if we're turning it off
+            if newAttributes[.underlineStyle] == nil {
+                textView.textStorage.removeAttribute(.underlineStyle, range: selectedRange)
+            } else {
+                textView.textStorage.addAttributes(newAttributes, range: selectedRange)
+            }
+        } else if style == .strikethrough {
+            // For strikethrough, we need to remove the attribute if we're turning it off
+            if newAttributes[.strikethroughStyle] == nil {
+                textView.textStorage.removeAttribute(.strikethroughStyle, range: selectedRange)
+            } else {
+                textView.textStorage.addAttributes(newAttributes, range: selectedRange)
+            }
+        } else {
+            textView.textStorage.addAttributes(newAttributes, range: selectedRange)
+        }
+        
+        attributedString = textView.attributedText ?? NSAttributedString()
+    }
+    
+    public class Coordinator: NSObject, UITextViewDelegate {
+        var parent: RichTextEditor
+        
+        init(_ parent: RichTextEditor) {
+            self.parent = parent
+        }
+        
+        public func textViewDidChange(_ textView: UITextView) {
+            parent.attributedString = textView.attributedText
+        }
+    }
+}
+
+enum TextStyle {
+    case bold, italic, underline, strikethrough
+}
+
+extension UIFont {
+    var isBold: Bool {
+        return fontDescriptor.symbolicTraits.contains(.traitBold)
+    }
+    
+    var isItalic: Bool {
+        return fontDescriptor.symbolicTraits.contains(.traitItalic)
+    }
+    
+    func withBold() -> UIFont {
+        return withTrait(.traitBold)
+    }
+    
+    func withoutBold() -> UIFont {
+        return withoutTrait(.traitBold)
+    }
+    
+    func withItalic() -> UIFont {
+        return withTrait(.traitItalic)
+    }
+    
+    func withoutItalic() -> UIFont {
+        return withoutTrait(.traitItalic)
+    }
+    
+    private func withTrait(_ trait: UIFontDescriptor.SymbolicTraits) -> UIFont {
+        guard let descriptor = fontDescriptor.withSymbolicTraits(fontDescriptor.symbolicTraits.union(trait)) else {
+            return self
+        }
+        return UIFont(descriptor: descriptor, size: 0)
+    }
+    
+    private func withoutTrait(_ trait: UIFontDescriptor.SymbolicTraits) -> UIFont {
+        guard let descriptor = fontDescriptor.withSymbolicTraits(fontDescriptor.symbolicTraits.subtracting(trait)) else {
+            return self
+        }
+        return UIFont(descriptor: descriptor, size: 0)
+    }
 }
 
 // #Preview {
